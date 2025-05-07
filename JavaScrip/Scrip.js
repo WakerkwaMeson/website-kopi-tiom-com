d49b02a3b227fdb753449b7742718526429cb944
Meson Wakerkwa <mesonofficial99@gmail.com>
2025-05-08 07:30:09 +0900

    Add files via upload

---
+++ JavaScrip/Scrip.js
@@ -0,0 +1,8 @@
+<script>
+  const hamburger = document.querySelector('#hamburger-menu');
+  const navMenu = document.querySelector('.navbar-nav');
+
+  hamburger.addEventListener('click', () = {
+    navMenu.classList.toggle('active')
+  });
+</script>
